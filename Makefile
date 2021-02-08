ba7d7608c1d66e949494b17525d29f171df35ac1
nikunicke <pimenoff.niklas@gmail.com>
2021-02-08 17:38:56 +0000

    :monocle_face: Examples - get-started

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ NAME = ftapi
 all:
 	go build -o ${NAME} ./cmd/ftapi
 
+get-started: examples/getStarted.go
+	go build -o get-started ./examples
+
 fclean:
 	rm -f $(NAME)
---
+++ examples/getStarted.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nikunicke/ftapi"
+)
+
+func main() {
+	// Create a new service client. Choose AuthorizationCode or
+	// ClientCredentials grant type.
+	service, err := ftapi.NewService(ftapi.AuthorizationCode)
+	if err != nil {
+		log.Fatalf("Authentication failed: %v", err)
+	}
+
+	// Get your user data
+	me, err := service.Me()
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
+	fmt.Printf("%d - %s %s\n", me.ID, me.FirstName, me.LastName)
+
+	// RAW response data from me.ServerResponse.Header | .Body | .StatusCode.
+	// ServerResponse is included in all direct responses.
+	fmt.Println(me.ServerResponse.StatusCode)
+
+	// Get user by ID
+	userService := ftapi.Users(service)
+	aUser, err := userService.Get("59634").Do()
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
+	// Get user levels (piscine/42/...)
+	for _, cursus := range aUser.CursusUsers {
+		fmt.Println(cursus.Cursus.Name, cursus.Level)
+	}
+
+	// Get page of users from Hive Helsinki
+	usersCaller := userService.List()
+	page, err := usersCaller.P("campus_id", []string{"13"}).Do()
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
+	// Get following pages
+	for len(page.Users) > 0 {
+		fmt.Println(len(page.Users))
+		page, err = usersCaller.PageToken(page.NextPage).Do()
+		if err != nil {
+			log.Fatalf("Request failed: %v", err)
+		}
+		time.Sleep(1 * time.Second) // Rate-limit
+	}
+}
